3f3be58bf7860440641241b8c593c657b93c2e66
Abubakar suraj <145049374+Aboosuraj@users.noreply.github.com>
2026-06-01 02:27:57 +0200

    Create WatchlistPage component for user tokens

---
+++ app/watchlist/page.tsx
@@ -0,0 +1,8 @@
+export default function WatchlistPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Watchlist</h1>
+      <p>Your favorite tokens will appear here.</p>
+    </div>
+  );
+}
